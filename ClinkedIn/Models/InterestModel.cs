7b9cb3510fd920041498f2f82b99e31c03f391f8
Andy Million <amillion3@gmail.com>
2018-10-02 18:03:36 -0500

    put interest type inside of interest-model class

--- ClinkedIn/Models/InterestModel.cs
+++ ClinkedIn/Models/InterestModel.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ClinkedIn.Models
 {
     public class InterestModel
     {
         public InterestType Type { get; set; }
+
+        public enum InterestType
+        {
+            Reading,
+            Romance,
+            Pruno,
+            Sports,
+            Contraband,
+            Gambling,
+            Republican,
+            Democrat,
+        }
     }
 
-    public enum InterestType
-    {
-        Reading,
-        Romance,
-        Pruno,
-        Sports,
-        Contraband,
-        Gambling,
-        Republican,
-        Democrat,
-    }
 }
