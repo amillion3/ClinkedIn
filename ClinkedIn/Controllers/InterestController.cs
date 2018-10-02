e891c8b7487ab2029a442854e6e0e999fba622f8
Andy Million <amillion3@gmail.com>
2018-10-01 23:08:25 -0500

    Added interest model and controller. controller has 2 GET methods, getAllFavorites() and getSingleFavorite()

--- ClinkedIn/Controllers/InterestController.cs
+++ ClinkedIn/Controllers/InterestController.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClinkedIn.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinkedIn.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class InterestController : Controller
     {
-        public IActionResult Index()
+        static List<InterestType> Interests = new List<InterestType>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<InterestType>> GetAllFavorites()
         {
-            return View();
+            return Interests;
+        }
+
+        [HttpGet("api/interest/{name}")]
+        public ActionResult<IEnumerable<InterestType>> GetSingleFavorite(string name)
+        {
+            var matchingInterest = 
+                Interests.Where( interest => interest.Equals(name) );
+            // why does this return work?
+            return Ok(matchingInterest);
         }
     }
 }
