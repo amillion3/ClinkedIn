7b8f878c0f7d42d9459c59ec8923a853b54073cd
Andy Million <amillion3@gmail.com>
2018-10-04 22:47:26 -0500

    fixed the broken _alcatraz object.

--- ClinkedIn/Controllers/InmateController.cs
+++ ClinkedIn/Controllers/InmateController.cs
@@ -7,85 +7,19 @@ using ClinkedIn.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Internal;
 
-
 namespace ClinkedIn.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class InmateController : Controller
     {
-        static List<Inmate> Inmates;
-
-        static InmateController()
-        {
-            //Inmates = new List<Inmate>
-            //{
-            //    new Inmate {Id = 1, Name = "Anthony Weiner", Conviction = "Sending sexually explicit photos of himself to a 15-year-old girl", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>(), Services = new Dictionary<string, double>()},
-            //    new Inmate {Id = 2, Name = "Chaka Fattah", Conviction = "Convicted on 23 counts of racketeering, fraud, and other corruption charges.", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>(), Services = new Dictionary<string, double>()},
-            //    new Inmate {Id = 3, Name = "Mark E. Fuller", Conviction = "Found guilty of domestic violence and sentenced to 24 weeks of family and domestic training and forced to resign his position.", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>(), Services = new Dictionary<string, double>()},
-            //    new Inmate {Id = 4, Name = "J. Michael Farren", Conviction = "Convicted of the attempted murder of his wife.", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>(), Services = new Dictionary<string, double>()},
-            //    new Inmate {Id = 5, Name = "Duke Cunningham", Conviction = "Pleaded guilty to charges of conspiracy to commit bribery, mail fraud, wire fraud and tax evasion", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>(), Services = new Dictionary<string, double>()},
-            //    new Inmate {Id = 6, Name = "Bill Janklow", Conviction = "Convicted of second-degree manslaughter for running a stop sign and killing a motorcyclist.", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>(), Services = new Dictionary<string, double>()},
-            //    new Inmate {Id = 7, Name = "Wade Sanders", Conviction = "Sentenced to 37 months in prison on one charge of possession of child pornography.", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>(), Services = new Dictionary<string, double>()},
-            //    new Inmate {Id = 8, Name = "Austin Murphy", Conviction = "Convicted of one count of voter fraud for filling out absentee ballots for members of a nursing home.", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>(), Services = new Dictionary<string, double>()},
-            //    new Inmate {Id = 9, Name = "Michael Grimm", Conviction = "Pleaded guilty of felony tax evasion.", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>(), Services = new Dictionary<string, double>()}
-            //};
-
-            //Inmates[0].Services.Add("Shiv disposal", 5.00);
-            //Inmates[1].Services.Add("Enforcer", 20.00);
-            //Inmates[2].Services.Add("Contraband Dealer", 10.00);
-            //Inmates[3].Services.Add("Stylist", 3.00);
-            //Inmates[4].Services.Add("Librarian", 2.00);
-            //Inmates[5].Services.Add("Cafeteria Worker", 4.00);
-            //Inmates[6].Services.Add("Laundry", 1.00);
-            //Inmates[7].Services.Add("Tattoo Artist", 8.00);
-            //Inmates[8].Services.Add("Hooch Man", 9.00);
-
-            //Inmates[0].Interests.Add(Interests.Romance);
-            //Inmates[0].Interests.Add(Interests.Reading);
-            //Inmates[0].Interests.Add(Interests.Democrat);
-            //Inmates[1].Interests.Add(Interests.Reading);
-            //Inmates[1].Interests.Add(Interests.Contraband);
-            //Inmates[1].Interests.Add(Interests.Democrat);
-            //Inmates[2].Interests.Add(Interests.Pruno);
-            //Inmates[2].Interests.Add(Interests.Gambling);
-            //Inmates[2].Interests.Add(Interests.Republican);
-            //Inmates[3].Interests.Add(Interests.Sports);
-            //Inmates[3].Interests.Add(Interests.Contraband);
-            //Inmates[3].Interests.Add(Interests.Republican);
-            //Inmates[4].Interests.Add(Interests.Contraband);
-            //Inmates[4].Interests.Add(Interests.Pruno);
-            //Inmates[4].Interests.Add(Interests.Republican);
-            //Inmates[5].Interests.Add(Interests.Gambling);
-            //Inmates[5].Interests.Add(Interests.Romance);
-            //Inmates[5].Interests.Add(Interests.Republican);
-            //Inmates[6].Interests.Add(Interests.Reading);
-            //Inmates[6].Interests.Add(Interests.Sports);
-            //Inmates[6].Interests.Add(Interests.Democrat);
-            //Inmates[7].Interests.Add(Interests.Contraband);
-            //Inmates[7].Interests.Add(Interests.Romance);
-            //Inmates[7].Interests.Add(Interests.Democrat);
-            //Inmates[8].Interests.Add(Interests.Reading);
-            //Inmates[8].Interests.Add(Interests.Pruno);
-            //Inmates[8].Interests.Add(Interests.Republican);
-        }
-
-
-
         [HttpGet("inmates")]
         public ActionResult<List<Inmate>> GetAll([FromQuery] string service, string interest)
         {
-            
-            //var results = Inmates;
-            //foreach (var r in results)
-            //{
-            //    _alcatraz.AddNewInmateInStorage(r);
-            //}
-
-            return Ok();
+            // not sure what is working/broken
             if (service != null)
             {
-                //return _alcatraz.GetAllFromStorage.Where(inmate => inmate.Services.ContainsKey(service)).ToList();
+                return _alcatraz.GetAllFromStorage.Where(inmate => inmate.Services.ContainsKey(service)).ToList();
             }
             if (interest != null)
             {
@@ -131,7 +65,6 @@ namespace ClinkedIn.Controllers
         }
 
         // storage
-        // whaaat. i don't know what this does
         private readonly InmateStorage _alcatraz;
         public InmateController()
         {
--- ClinkedIn/DataAccess/InmateStorage.cs
+++ ClinkedIn/DataAccess/InmateStorage.cs
@@ -9,55 +9,111 @@ namespace ClinkedIn.DataAccess
     // store and return inmates on demand
     public class InmateStorage
     {
-        public void alcatraz()
+        static List<Inmate> _alcatraz = new List<Inmate>()
         {
-            List<Inmate> Inmates = new List<Inmate>()
-            {
-            new Inmate {Id = 1, Name = "anthony weiner", Conviction = "sending sexually explicit photos of himself to a 15-year-old girl", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>{Interests.Contraband }, Services = new Dictionary<string, double>()},
-            new Inmate {Id = 2, Name = "chaka fattah", Conviction = "convicted on 23 counts of racketeering, fraud, and other corruption charges.", Friends = new List<Inmate>(), Enemies = new List<Inmate>(), Interests = new List<Interests>{Interests.Reading }, Services = new Dictionary<string, double>()},
-            //new inmate {id = 3, name = "mark e. fuller", conviction = "found guilty of domestic violence and sentenced to 24 weeks of family and domestic training and forced to resign his position.", friends = new list<inmate>(), enemies = new list<inmate>(), interests = new list<interests>(), services = new dictionary<string, double>()},
-            //new inmate {id = 4, name = "j. michael farren", conviction = "convicted of the attempted murder of his wife.", friends = new list<inmate>(), enemies = new list<inmate>(), interests = new list<interests>(), services = new dictionary<string, double>()},
-            //new inmate {id = 5, name = "duke cunningham", conviction = "pleaded guilty to charges of conspiracy to commit bribery, mail fraud, wire fraud and tax evasion", friends = new list<inmate>(), enemies = new list<inmate>(), interests = new list<interests>(), services = new dictionary<string, double>()},
-            //new inmate {id = 6, name = "bill janklow", conviction = "convicted of second-degree manslaughter for running a stop sign and killing a motorcyclist.", friends = new list<inmate>(), enemies = new list<inmate>(), interests = new list<interests>(), services = new dictionary<string, double>()},
-            //new inmate {id = 7, name = "wade sanders", conviction = "sentenced to 37 months in prison on one charge of possession of child pornography.", friends = new list<inmate>(), enemies = new list<inmate>(), interests = new list<interests>(), services = new dictionary<string, double>()},
-            //new inmate {id = 8, name = "austin murphy", conviction = "convicted of one count of voter fraud for filling out absentee ballots for members of a nursing home.", friends = new list<inmate>(), enemies = new list<inmate>(), interests = new list<interests>(), services = new dictionary<string, double>()},
-            //new inmate {id = 9, name = "michael grimm", conviction = "pleaded guilty of felony tax evasion.", friends = new list<inmate>(), enemies = new list<inmate>(), interests = new list<interests>(), services = new dictionary<string, double>()}
-            };
-            Inmates[0].Services.Add("Shiv disposal", 5.00);
-            Inmates[1].Services.Add("Enforcer", 20.00);
-            //Inmates[2].Services.Add("Contraband Dealer", 10.00);
-            //Inmates[3].Services.Add("Stylist", 3.00);
-            //Inmates[4].Services.Add("Librarian", 2.00);
-            //Inmates[5].Services.Add("Cafeteria Worker", 4.00);
-            //Inmates[6].Services.Add("Laundry", 1.00);
-            //Inmates[7].Services.Add("Tattoo Artist", 8.00);
-            //Inmates[8].Services.Add("Hooch Man", 9.00);
+            new Inmate {
+                Id = 1,
+                Name = "Anthony Weiner",
+                Conviction = "Sending sexually explicit photos of himself to a 15-year-old girl",
+                Friends = new List<Inmate>(),
+                Enemies = new List<Inmate>(),
+                Interests = new List<Interests>{ Interests.Romance, Interests.Reading, Interests.Democrat },
+                Services = new Dictionary<string, double>(){ {"Shiv disposal", 5.00 } }
+                },
+            new Inmate {
+                Id = 2,
+                Name = "Chaka Fattah",
+                Conviction = "Convicted on 23 counts of racketeering, fraud, and other corruption charges.",
+                Friends = new List<Inmate>(),
+                Enemies = new List<Inmate>(),
+                Interests = new List<Interests>{ Interests.Contraband, Interests.Reading, Interests.Democrat },
+                Services = new Dictionary<string, double>(){ {"Laundry", 1.00 } }
+                },
+            new Inmate {
+                Id = 3,
+                Name = "Mark E. Fuller",
+                Conviction = "Found guilty of domestic violence and sentenced to 24 weeks of family and domestic training and forced to resign his position.",
+                Friends = new List<Inmate>(),
+                Enemies = new List<Inmate>(),
+                Interests = new List<Interests>{ Interests.Pruno, Interests.Gambling, Interests.Republican },
+                Services = new Dictionary<string, double>(){ {"Hooch Man", 25.00 } }
+            },
+            new Inmate {
+                Id = 4,
+                Name = "J. Michael Farren",
+                Conviction = "Convicted of the attempted murder of his wife.",
+                Friends = new List<Inmate>(),
+                Enemies = new List<Inmate>(),
+                Interests = new List<Interests>{ Interests.Sports, Interests.Contraband, Interests.Republican },
+                Services = new Dictionary<string, double>(){ {"Enforcer", 20.00 } }
+            },
+            new Inmate {
+                Id = 5,
+                Name = "Duke Cunningham",
+                Conviction = "Pleaded guilty to charges of conspiracy to commit bribery, mail fraud, wire fraud and tax evasion",
+                Friends = new List<Inmate>(),
+                Enemies = new List<Inmate>(),
+                Interests = new List<Interests>{ Interests.Contraband, Interests.Pruno, Interests.Republican },
+                Services = new Dictionary<string, double>(){ {"Contraband Dealer", 10.00 } }
+            },
+            new Inmate {
+                Id = 6,
+                Name = "Bill Janklow",
+                Conviction = "Convicted of second-degree manslaughter for running a stop sign and killing a motorcyclist.",
+                Friends = new List<Inmate>(),
+                Enemies = new List<Inmate>(),
+                Interests = new List<Interests>{ Interests.Gambling, Interests.Romance, Interests.Republican },
+                Services = new Dictionary<string, double>(){ {"Stylist", 3.00 } }
+            },
+            new Inmate {
+                Id = 7,
+                Name = "Wade Sanders",
+                Conviction = "Sentenced to 37 months in prison on one charge of possession of child pornography.",
+                Friends = new List<Inmate>(),
+                Enemies = new List<Inmate>(),
+                Interests = new List<Interests>{ Interests.Reading, Interests.Sports, Interests.Democrat },
+                Services = new Dictionary<string, double>(){ {"Librarian", 200.00 } }
+            },
+            new Inmate {
+                Id = 8,
+                Name = "Austin Murphy",
+                Conviction = "Convicted of one count of voter fraud for filling out absentee ballots for members of a nursing home.",
+                Friends = new List<Inmate>(),
+                Enemies = new List<Inmate>(),
+                Interests = new List<Interests>{ Interests.Contraband, Interests.Romance, Interests.Democrat },
+                Services = new Dictionary<string, double>(){ {"Cafeteria Worker", 5.00 } }
+            },
+            new Inmate {
+                Id = 9,
+                Name = "Michael Grimm",
+                Conviction = "Pleaded guilty of felony tax evasion.",
+                Friends = new List<Inmate>(),
+                Enemies = new List<Inmate>(),
+                Interests = new List<Interests>{ Interests.Pruno, Interests.Reading, Interests.Republican },
+                Services = new Dictionary<string, double>(){ {"Tattoo Artist", 15.00 } }
+            }
+        };
+
+        public void AddNewInmateInStorage(Inmate inmate)
+        {
+            // does inmate have an id? if no, find the highest id and add 1
+            // if not, set the id to 1 (IE, the very first inmate)
+            inmate.Id = _alcatraz.Any() ?
+                _alcatraz.Max(i => i.Id) + 1
+                : 1;
+            _alcatraz.Add(inmate);
+            Console.WriteLine(inmate);
         }
 
+        public IEnumerable<Inmate> GetAllFromStorage()
+        {
+            return _alcatraz;
+        }
 
-
-
-            public void AddNewInmateInStorage(Inmate inmate)
-            {
-                // does inmate have an id? if no, find the highest id and add 1
-                // if not, set the id to 1 (IE, the very first inmate)
-                inmate.Id = alcatraz.Inmates.Any() ?
-                    _alcatraz.Max(i => i.Id) + 1
-                    : 1;
-                _alcatraz.Add(inmate);
-                Console.WriteLine(inmate);
-            }
-
-            public IEnumerable<Inmate> GetAllFromStorage()
-            {
-                return _alcatraz;
-            }
-
-            public Inmate GetById(int id)
-            {
-                // Find the first matching id from _alcatraz
-                return _alcatraz.First(i => i.Id == id);
-            }
-        
+        public Inmate GetById(int id)
+        {
+            // Find the first matching id from _alcatraz
+            return _alcatraz.First(i => i.Id == id);
+        }
     }
 }
